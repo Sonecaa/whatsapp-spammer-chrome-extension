e540c9b97e3f17c8acbdd97a8d31177c4a7d255c
Marcelo Assis <marcelo.assis.santos@live.com>
2019-06-05 14:32:26 +0000

    validations and some fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,30 @@
 
- document.forms["main-form"].addEventListener("submit", send_form);
+document.forms["main-form"].addEventListener("submit", send_form);
+$('#alert-box').hide();
+
 
 function send_form(e) {
-  
+
   e.preventDefault();
 
   var form = document.forms["main-form"];
 
-  var message = form["message-input"];
+  var message = form["message-input"].value;
 
-  var repeat = form["repeat-input"];
+  var repeat = form["repeat-input"].value;
 
-  var delay = form["delay-input"];
+  var delay = form["delay-input"].value;
 
   var sended = 0;
 
   var interval = setInterval(function () {
-
-    sended += 1;
 
     if(sended == repeat){
       clearInterval(interval);
     }
 
     send_message(message);
+    sended += 1;
 
   }, delay);
 
@@ -41,9 +42,20 @@ function send_message(message) {
 
   var textbox = document.querySelector('div._2S1VP');
 
-  textbox.textContent = message;
-  textbox.dispatchEvent(event);
+  if (textbox) {
 
-  document.querySelector("button._35EW6").click();
+    textbox.textContent = message;
+    textbox.dispatchEvent(event);
+
+    document.querySelector("button._35EW6").click();
+
+  } else {
+
+    $('#alert-box').html('Open chat in Whatsapp first...')
+    $('#alert-box').show(2000);
+    setTimeout( function() { $('#alert-box').hide('2000'); } , 6000);
+
+
+  }
 
 }
